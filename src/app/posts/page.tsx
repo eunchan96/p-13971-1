bdfb53709a80b655344e1e6fcedbb45a0356f368
eunchan96 <taegwon02@gmail.com>
2025-07-01 14:50:57 +0900

    posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Page() {
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    setPosts([
+      { id: 1, title: "명언 1" },
+      { id: 2, title: "명언 2" },
+      { id: 3, title: "명언 3" },
+    ]);
+  }, []);
+
   return (
     <>
       <h1>글 목록</h1>
 
       <ul>
-        <li>명언 1</li>
-        <li>명언 2</li>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
       </ul>
     </>
   );
