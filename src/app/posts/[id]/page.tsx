11ce26ff492389fbb2442f4847bf7b708365ee4c
eunchan96 <taegwon02@gmail.com>
2025-07-02 11:29:57 +0900

    PostComment dto, lists

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { apiFetch } from "@/lib/backend/client";
-import type { PostWithContentDto } from "@/type/post";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react";
 
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
   const { id } = use(params);
-  const [post, setPost] = useState<PostWithContentDto | null>(null);
   const router = useRouter();
+
+  const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(null);
 
   const deletePost = (id: number) => {
     apiFetch(`/api/v1/posts/${id}`, {
@@ -22,6 +24,7 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
 
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`).then(setPost);
+    apiFetch(`/api/v1/posts/${id}/comments`).then(setPostComments);
   }, []);
 
   if (post == null) {
@@ -41,6 +44,15 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
           onClick={() => confirm(`${post.id}번 글을 정말 삭제하시겠습니까?`) && deletePost(post.id)}>삭제</button>
         <Link className="border rounded p-2" href={`/posts/${post.id}/edit`}>수정</Link>
       </div>
+
+      <h2>댓글 목록</h2>
+      {postComments == null && <div>댓글 로딩중...</div>}
+      {postComments != null && postComments.length === 0 && <div>댓글이 없습니다.</div>}
+      {postComments != null && postComments.length > 0 && postComments.map((comment) => (
+        <div key={comment.id}>
+          <div>{comment.content}</div>
+        </div>
+      ))}
     </>
   );
 }
--- src/type/post.ts
+++ src/type/post.ts
@@ -1,8 +1,17 @@
 export interface PostDto {
     id: number;
+    createDate: string;
+    modifyDate: string;
     title: string;
 }
 
 export interface PostWithContentDto extends PostDto {
+    content: string;
+}
+
+export interface PostCommentDto {
+    id: number;
+    createDate: string;
+    modifyDate: string;
     content: string;
 }
