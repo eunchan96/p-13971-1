44c3eef86988a98eaa5a5cb14bb62fd1c77ebeb2
eunchan96 <taegwon02@gmail.com>
2025-07-02 10:54:31 +0900

    Post delete

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,11 +2,23 @@
 
 import { apiFetch } from "@/lib/backend/client";
 import type { PostWithContentDto } from "@/type/post";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react";
 
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
   const { id } = use(params);
   const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const router = useRouter();
+
+  const deletePost = (id: number) => {
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+      router.replace("/posts");
+    });
+  };
 
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`).then(setPost);
@@ -24,6 +36,11 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
       <div>제목 : {post.title}</div>
       <div style={{ whiteSpace: "pre-line" }}>내용 : {post.content}</div>
 
+      <div className="flex gap-2">
+        <button className="border rounded p-2 cursor-pointer" 
+          onClick={() => confirm(`${post.id}번 글을 정말 삭제하시겠습니까?`) && deletePost(post.id)}>삭제</button>
+        <Link className="border rounded p-2" href={`/posts/${post.id}/modify`}>수정</Link>
+      </div>
     </>
   );
 }
