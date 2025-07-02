a6031214df4b85ce9950af0ce97fe8d106a04308
eunchan96 <taegwon02@gmail.com>
2025-07-02 10:25:48 +0900

    Post write (2)

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -9,24 +9,41 @@ export default function Page() {
         const title = form.elements.namedItem("title") as HTMLInputElement;
         const content = form.elements.namedItem("content") as HTMLTextAreaElement;
 
+        title.value = title.value.trim();
+
         if (title.value.length === 0) {
             alert("제목을 입력해주세요.");
             title.focus();
             return;
         }
 
+        content.value = content.value.trim();
+
         if (content.value.length === 0) {
             alert("내용을 입력해주세요.");
             content.focus();
             return;
         }
+
+        fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({
+                title: title.value,
+                content: content.value,
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => alert(data.msg));
     };
 
     return (
         <>
             <h1>글쓰기</h1>
 
-            <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+            <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
                 <input className="border rounded p-2" type="text" name="title" placeholder="제목" />
                 <textarea className="border rounded p-2" name="content" placeholder="내용" />
                 <button className="border rounded p-2" type="submit">저장</button>
